696bcb6d0598a84e48f796c922370dd74da3ab7f
nikhilakki <nikhilakki@users.noreply.github.com>
2017-05-27 11:57:25 +0000

    Done

--- build.py
+++ build.py
@@ -2,8 +2,11 @@ def solution(list):
     '''
     Enter your code here
     '''
+
     unique = []
     for x in list:
         if x not in unique:
             unique.append(x)
     return (unique)
+
+print solution([12,34,565,77,77,77])
