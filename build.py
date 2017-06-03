3d6b05e1ac483be744d1ae3de52adf3c6eb9bee8
varunpanicker <varunpanicker@users.noreply.github.com>
2017-06-03 11:30:02 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 def solution(list):
+    list=[1,2,3,4,4,5,5]
     unique = []
     for x in list:
         if x not in unique:
             unique.append(x)
     return (unique)
-print solution([1,2,3,4,4,5,5])
+print solution(list)
