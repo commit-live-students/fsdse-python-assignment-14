f8cffed8b547c78ceb861605b70ff23b94c7b103
adinathauti <adinathauti@gmail.com>
2017-06-16 06:41:08 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 def solution(list):
-    '''
-    Enter your code here
-    '''
+    seen = set()
     unique = []
     for x in list:
-        if x not in unique:
+        if x not in seen:
+            seen.add(x)
             unique.append(x)
+
+    print unique
     return (unique)
+
+solution([1,2,3,1,2,3,4,4,6,2,7,8])
