e6628ff53c1f3e6cd9f176dadb62acbcc0cd6d05
sandiprb <baradiya.sandip@gmail.com>
2017-08-10 18:38:43 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,2 @@
 def solution(list):
-    '''
-    Enter your code here
-    '''
-    unique = []
-    for x in list:
-        if x not in unique:
-            unique.append(x)
-    return (unique)
+    return set(list)
